6d4983f42cf764449be34f3bba698cf2d706e9a3
Lolymoly <teetat.wis@gmail.com>
2024-04-04 15:31:46 +0700

    feat: logout api

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -27,26 +27,47 @@ exports.register = async (req, res, next) => {
 };
 
 exports.login = async (req, res, next) => {
-	const { email, password } = req.body;
-	if (!email || !password) {
-		return res
-			.status(400)
-			.json({ success: false, msg: "Please provide email and password" });
-	}
+	try {
+		const { email, password } = req.body;
+		if (!email || !password) {
+			return res
+				.status(400)
+				.json({ success: false, msg: "Please provide email and password" });
+		}
 
-	const user = await User.findOne({ email }).select("+password");
-	if (!user) {
-		return res.status(401).json({ success: false, msg: "Invalid credentials" });
-	}
+		const user = await User.findOne({ email }).select("+password");
+		if (!user) {
+			return res
+				.status(401)
+				.json({ success: false, msg: "Invalid credentials" });
+		}
 
-	const isMatch = await user.matchPassword(password);
-	if (!isMatch) {
-		return res.status(401).json({ success: false, msg: "Invalid credentials" });
-	}
+		const isMatch = await user.matchPassword(password);
+		if (!isMatch) {
+			return res
+				.status(401)
+				.json({ success: false, msg: "Invalid credentials" });
+		}
 
-	// const token = user.getSignedJwtToken();
-	// res.status(200).json({ success: true, token });
-	sendTokenResponse(user, 200, res);
+		// const token = user.getSignedJwtToken();
+		// res.status(200).json({ success: true, token });
+		sendTokenResponse(user, 200, res);
+	} catch (error) {
+		res.status(401).json({ success: false, msg: error.message });
+	}
+};
+
+//Logout user
+//@desc Clear token cookie
+//@route GET /api/v1/auth/logout
+//@access Private
+
+exports.logout = async (req, res, next) => {
+	res.cookie("token", "none", {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true,
+	});
+	res.status(200).json({ success: true, data: {} });
 };
 
 //Get token from model, create cookie and send response
