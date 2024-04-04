159c38cc716e9d05f6e4581a97ebf5a5ea389db7
Lolymoly <teetat.wis@gmail.com>
2024-04-04 16:51:31 +0700

    feat: add rate-limit

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const cors = require("cors");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const { xss } = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
 
 dotenv.config({ path: "./config/config.env" });
 connectDB();
@@ -27,6 +28,12 @@ app.use(cookieParser());
 app.use(mongoSanitize());
 app.use(helmet());
 app.use(xss());
+
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // 10 minutes
+	max: 100,
+});
+app.use(limiter);
 
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/auth", auth);
