726b656e9930b3dc6451bb28407bc10f4b5534c7
Lolymoly <teetat.wis@gmail.com>
2024-04-04 15:42:13 +0700

    feat: add helmet

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const appointments = require("./routes/appointments");
 const cors = require("cors");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 
 dotenv.config({ path: "./config/config.env" });
 connectDB();
@@ -23,6 +24,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 app.use(mongoSanitize());
+app.use(helmet());
 
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/auth", auth);
